599abef56129dc40d455585f1444e9630212c6b0
surajmishra1007 <surajmishra1007@gmail.com>
2018-07-24 13:43:43 +0530

    Assignment 7.2

---
+++ main.js
@@ -0,0 +1,13 @@
+var num = 5,
+    i = 1; //variable declaration
+console.log('Using for loop');
+for (var i = 1; i <= 10; i++) { //for loop
+    console.log(num * i); //console output
+}
+i = 1;
+console.log('Using for While');
+while (i <= 10) //while loop
+{
+    console.log(num * i); //console output
+    i++;
+}
